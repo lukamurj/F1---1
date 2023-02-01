294eae5e7862cd975e7b44c3768cd8c3d2605770
lukamurj <lukamurjikneli2006@gmail.com>
2023-02-01 15:15:08 +0400

    first js assignment added

---
+++ scrypt.js
@@ -0,0 +1,40 @@
+// 1.1
+let counter = 0;
+counter++;
+alert(counter);
+
+// 1.2
+let num;
+num = 5;
+alert(num);
+num = 10;
+alert(num);
+
+// 1.3
+let b;
+b = 10 + 20 + 30;
+alert(b);
+
+// 1.4
+let A = 100,
+  B = 40,
+  c = A - B,
+  d = 20,
+  result = c + d;
+alert(result);
+
+// 1.5
+// a = 13
+
+// 1.6
+let C;
+C = 3;
+alert(C);
+
+// 1.7
+// a = 72
+
+// 1.8
+let birthDate;
+birthDate = "08.01.2003";
+alert(birthDate);
